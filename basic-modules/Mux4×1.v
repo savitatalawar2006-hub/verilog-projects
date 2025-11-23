a4eb1a9ed13c28e0ec65febd56dd3d4a452f74c7
SAVITA TALAWAR <savitatalwar2006@gmail.com>
2025-11-23 07:07:38 +0530

    Add 4-to-1 multiplexer module

---
+++ basic-modules/Mux4×1.v
@@ -0,0 +1,10 @@
+module mux4x1(
+    input  wire [3:0] d,
+    input  wire [1:0] sel,
+    output wire y
+);
+    assign y = (sel == 2'b00) ? d[0] :
+               (sel == 2'b01) ? d[1] :
+               (sel == 2'b10) ? d[2] : 
+                                d[3];
+endmodule
